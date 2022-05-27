3124ed9c7168d75c59f63629d6ba11fbf5b792f7
ChaeGyeong <104820375+ChaeGyeong@users.noreply.github.com>
2022-05-27 15:50:19 +0900

    Add files via upload

---
+++ item/MontblancItems.java
@@ -0,0 +1,132 @@
+package item;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import frame.Main;
+
+public class MontblancItems extends JFrame {
+	
+	private ImageIcon[] imgs = {
+			new ImageIcon("itemImages/montblanc01.jpg"),
+			new ImageIcon("itemImages/montblanc02.jpg"),
+			new ImageIcon("itemImages/montblanc03.jpg"),
+			new ImageIcon("itemImages/montblanc04.jpg"),
+			new ImageIcon("itemImages/montblanc05.jpg"),
+			new ImageIcon("itemImages/montblanc06.jpg"),
+			new ImageIcon("itemImages/montblanc07.jpg"),
+			new ImageIcon("itemImages/montblanc08.jpg"),
+			new ImageIcon("itemImages/montblanc09.jpg"),
+			};
+	
+	public MontblancItems(String title, int width, int height) {
+		setTitle(title);
+	    setSize(width, height);
+	    setLocationRelativeTo(null);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setLayout(new BorderLayout());  
+	    
+	    setScrollPanel();
+	    
+	    setVisible(true);
+	}
+	
+	private void setScrollPanel() {
+	        JPanel panel = new JPanel();      
+	        panel.setPreferredSize(new Dimension(400, 1500));   // 패널에 사이즈 설정 
+	        panel.setBackground(Color.WHITE);
+	        panel.setLayout(new FlowLayout());
+	        
+	        JScrollPane sp = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	         
+	        add(sp);
+	        
+	        JPanel container = new JPanel();
+	        container.setLayout(new BorderLayout());
+	        container.setBackground(Color.WHITE);
+	        
+	        //로고이미지,메뉴바----------------
+	        ImageIcon logo = new ImageIcon("images/javacoco.png");
+	        JLabel logoLabel = new JLabel(logo);
+	        logoLabel.setHorizontalAlignment(JLabel.CENTER);
+	        JMenuBar mb = new JMenuBar();
+	        mb.setLayout(new FlowLayout(FlowLayout.CENTER, 235, 50));
+	        mb.setBackground(Color.WHITE);
+
+	        Font font = new Font("HY견명조", Font.BOLD, 25);
+	        Font font2 = new Font("HY견명조", Font.PLAIN, 18);
+	        JMenu menuCategory = new JMenu("category");
+	        
+	        menuCategory.setFont(font);
+	        
+	            
+	        JMenu menuSearch = new JMenu("search");
+	        menuSearch.setFont(font);
+	            
+	        JMenu menuMyPage = new JMenu("MyPage");
+	        menuMyPage.setFont(font);
+	        JMenuItem menuItemLogIn = new JMenuItem("Log In");
+	        menuItemLogIn.setFont(font2);
+	        menuMyPage.add(menuItemLogIn);
+	        JMenuItem menuItemJoinUs = new JMenuItem("Join Us");
+	        menuItemJoinUs.setFont(font2);
+	        menuMyPage.add(menuItemJoinUs);
+	        JMenuItem menuItemMyPage = new JMenuItem("My Page");
+	        menuItemMyPage.setFont(font2);
+	        menuMyPage.add(menuItemMyPage);
+	            
+	        JMenu menuCart = new JMenu("cart");
+	        menuCart.setFont(font);
+
+	        mb.add(menuCategory);
+	        mb.add(menuSearch);
+	        mb.add(menuMyPage);
+	        mb.add(menuCart);
+	        
+	        container.add(logoLabel, BorderLayout.NORTH);
+	        container.add(mb, BorderLayout.CENTER);
+	        
+	        panel.add(container);
+	        
+	        
+	        //상품 정렬
+	        JPanel panel01 = new JPanel();
+	        panel01.setPreferredSize(new Dimension(1800, 1000));
+		    panel01.setBackground(Color.white);
+	        panel01.setLayout(new GridLayout(3,3));
+	        
+	        JLabel textLabel = new JLabel("     Mont Blanc     ");
+	        textLabel.setFont(new Font(" ", ABORT, 50));
+	        
+	        for(int i=0; i<9; i++) {
+	        	
+	        JLabel item = new JLabel(imgs[i]);
+	        panel01.add(item);
+	        }
+	        panel.add(textLabel);
+	        panel.add(panel01);
+	        
+	        setVisible(true);
+	        
+	}
+
+	
+    public static void main(String[] args)
+      {
+    	new MontblancItems("JavaCoCo", 1000, 500);
+
+      }
+}
