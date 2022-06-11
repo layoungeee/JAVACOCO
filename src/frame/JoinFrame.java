897a0e62d5909d9b605ad8070dcf378f7898ce80
eunsol1010 <104820353+eunsol1010@users.noreply.github.com>
2022-06-12 01:37:41 +0900

    Update JoinFrame.java

--- src/frame/JoinFrame.java
+++ src/frame/JoinFrame.java
@@ -327,85 +327,104 @@ public class JoinFrame extends JFrame{
           
          
           
-      JPanel p = new JPanel();
-      p.setLayout(new GridLayout(0,2,20, 30));
-      p.setBackground(Color.WHITE);
-      JLabel name = new JLabel("이름");
-      TextField t1 = new TextField(30);
-      JLabel id = new JLabel("아이디");
-      TextField t2 = new TextField(30);
-      JLabel pw = new JLabel("패스워드");
-      TextField t3 = new TextField(30);
-      JLabel ad = new JLabel("주소");
-      TextField t4 = new TextField(30);
-      JLabel phone = new JLabel("전화번호");
-      TextField t5 = new TextField(30);
-      
-      p.add(name);
-      p.add(t1);
-      p.add(id);
-      p.add(t2);
-      p.add(pw);
-      p.add(t3);
-      p.add(phone);
-      p.add(t4);
-      p.add(ad);
-      p.add(t5);
+          JPanel p = new JPanel();
+          Font font3 = new Font("함초롬돋움", Font.BOLD, 15);
+          p.setLayout(new GridLayout(0,2,20, 30));
+	      p.setBackground(Color.WHITE);
+	      JLabel name = new JLabel("이름");
+	      name.setFont(font3);
+	      TextField t1 = new TextField(30);
+	      t1.setFont(font3);
+	      JLabel id = new JLabel("아이디");
+	      id.setFont(font3);
+	      TextField t2 = new TextField(30);
+	      t2.setFont(font3);
+	      JLabel pw = new JLabel("패스워드");
+	      pw.setFont(font3);
+	      TextField t3 = new TextField(30);
+	      t3.setFont(font3);
+	      JLabel ad = new JLabel("주소");
+	      ad.setFont(font3);
+	      TextField t4 = new TextField(30);
+	      t4.setFont(font3);
+	      JLabel phone = new JLabel("전화번호");
+	      phone.setFont(font3);
+	      TextField t5 = new TextField(30);
+	      t5.setFont(font3);
+	      
+	      p.add(name);
+	      p.add(t1);
+	      p.add(id);
+	      p.add(t2);
+	      p.add(pw);
+	      p.add(t3);
+	      p.add(phone);
+	      p.add(t4);
+	      p.add(ad);
+	      p.add(t5);
      
       
       
-      t3.setEchoChar('*');
-      JButton ok = new JButton("가입");
-      JButton no = new JButton("취소");
-      p.add(ok);
-      p.add(no);
-      
-      JPanel J = new JPanel();
-      J.setBackground(Color.WHITE);
-      Font f = new Font("HY견명조", Font.PLAIN, 50);
-      J.setPreferredSize(new Dimension(1100, 120));
-      JLabel Join = new JLabel("Join");
-      
-      Join.setFont(f); 
-      J.add(Join);
-      
-      panel.add(container);
-      panel.add(J);
-      panel.add(p);
-      
-      ok.addActionListener(new ActionListener() {
-
-  		@Override
-  		public void actionPerformed(ActionEvent e) {
-  			String id = t1.getText();
-  			String pw = t2.getText();
-  			
-  			JOptionPane.showMessageDialog(null, "회원가입 완료");
-  			
-  		}
-      	  
-        });
-        no.addActionListener(new ActionListener() {
-
-  		@Override
-  		public void actionPerformed(ActionEvent e) {
-  			new JoinFrame("JavaCoCo", 1000, 500);
-  			dispose();
-  		}
-      	  
-        });
-        ok.addActionListener(new ActionListener() {
-
-    		@Override
-    		public void actionPerformed(ActionEvent e) {
-    			new MainFrame("JavaCoCo", 1000, 500);
-    			dispose();
-    		}
-        	  
-          });
-
-   }
-
- 
-
-}
+	      t3.setEchoChar('*');
+	      JButton ok = new JButton("가입");
+	      ok.setFont(font3);
+	      ok.setBorderPainted(true);
+	      ok.setContentAreaFilled(false);
+	      ok.setFocusPainted(false);
+	      JButton no = new JButton("취소");
+	      no.setFont(font3);
+	      no.setBorderPainted(true);
+	      no.setContentAreaFilled(false);
+	      no.setFocusPainted(false);
+	      p.add(ok);
+	      p.add(no);
+	      
+	      JPanel J = new JPanel();
+	      J.setBackground(Color.WHITE);
+	      Font f = new Font("HY견명조", Font.PLAIN, 50);
+	      J.setPreferredSize(new Dimension(1100, 120));
+	      JLabel Join = new JLabel("Join");
+	      
+	      Join.setFont(f); 
+	      J.add(Join);
+	      
+	      panel.add(container);
+	      panel.add(J);
+	      panel.add(p);
+	      
+	      ok.addActionListener(new ActionListener() {
+	
+	  		@Override
+	  		public void actionPerformed(ActionEvent e) {
+	  			String id = t1.getText();
+	  			String pw = t2.getText();
+	  			
+	  			JOptionPane.showMessageDialog(null, "회원가입 완료");
+	  			
+	  		}
+	      	  
+	        });
+	        no.addActionListener(new ActionListener() {
+	
+	  		@Override
+	  		public void actionPerformed(ActionEvent e) {
+	  			new JoinFrame("JavaCoCo", 1000, 500);
+	  			dispose();
+	  		}
+	      	  
+	        });
+	        ok.addActionListener(new ActionListener() {
+	
+	    		@Override
+	    		public void actionPerformed(ActionEvent e) {
+	    			new MainFrame("JavaCoCo", 1000, 500);
+	    			dispose();
+	    		}
+	        	  
+	          });
+	
+	   }
+	
+	 
+	
+	}
